a1b098fc197f0c826babd34dbd875508de37e2c0
LynneDC <roselinedc1308@gmail.com>
2024-02-20 04:54:05 +0200

    user personal data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
-    """
-User passwords should NEVER be stored in plain text in a database.
-Implement a hash_password function that expects one string argument name password and returns a salted, hashed password, which is a byte string.
-Use the bcrypt package to perform the hashing (with hashpw)._
+"""
+Encrypting passwords
 """
 import bcrypt
 
-def hash_password(password):
-    # Generate a salt
-    salt = bcrypt.gensalt()
-    # Hash the password with the generated salt
-    hashed_password = bcrypt.hashpw(password.encode(), salt)
-    return hashed_password
 
-def is_valid(hashed_password, password):
-    # Validate the password against the hashed password
-      return bcrypt.checkpw(password.encode(), hashed_password)
+def hash_password(password: str) -> bytes:
+    """ Returns a salted, hashed password, which is a byte string """
+    encoded = password.encode()
+    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
+
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Validates the provided password matches the hashed password """
+    valid = False
+    encoded = password.encode()
+    if bcrypt.checkpw(encoded, hashed_password):
+        valid = True
+    return valid
--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,95 +1,91 @@
 #!/usr/bin/env python3
 """
-function called filter_datum:
- -returns the log message obfuscated
- ARGS:
-    -fields: list of tsrings representing all fields to obfuscate
-    -redaction:string represent what field will be  obfuscated
-    -message:  string represent the log line
-    -separator: str rep that guide in char separation in msg
-Use regex to replace occurance of certain field vals
-filter_datum is less than 5 line
-uses re.sub to perfom subsititution with single regex
+Module for handling Personal Data
 """
+from typing import List
 import re
 import logging
-import csv
-import os
+from os import environ
 import mysql.connector
-import mysql.connector
-from filtered_logger import get_logger, PII_FIELDS
-from get_db import get_db
-import bcrypt
 
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
+
+
+def get_logger() -> logging.Logger:
+    """ Returns a Logger Object """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a connector to a MySQL database """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    cnx = mysql.connector.connection.MySQLConnection(user=username,
+                                                     password=password,
+                                                     host=host,
+                                                     database=db_name)
+    return cnx
+
+
+def main():
+    """
+    Obtain a database connection using get_db and retrieves all rows
+    in the users table and display each row under a filtered format
+    """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    field_names = [i[0] for i in cursor.description]
+
+    logger = get_logger()
+
+    for row in cursor:
+        str_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, field_names))
+        logger.info(str_row.strip())
+
+    cursor.close()
+    db.close()
 
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-    """
+        """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: list):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ Filters values in incoming log records using filter_datum """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
 
-    def filter_datum(fields, redaction, message, separator):
-        return re.sub(separator.join(fields), redaction, message)
 
-    def get_logger():
-        logger = logging.getLogger('user_data')
-        logger.setLevel(logging.INFO)
-        logger.propagate = False
-
-        handler = logging.StreamHandler()
-        formatter = RedactingFormatter(PII_FIELDS)
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-
-        return logger
-
-    def get_db():
-        db_username = os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
-        db_password = os.environ.get('PERSONAL_DATA_DB_PASSWORD', '')
-        db_host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
-        db_name = os.environ['PERSONAL_DATA_DB_NAME']
-
-        db_connection = mysql.connector.connect(
-            user=db_username,
-            password=db_password,
-            host=db_host,
-            database=db_name
-        )
-
-        return db_connection
-
-    def main():
-        db = get_db()
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM users")
-
-        logger = get_logger()
-        for row in cursor:
-            message = "; ".join(f"{field}={value}"
-                                for field, value in
-                                zip(cursor.column_names, row))
-            logger.info(message)
-
-    def hash_password(password: str) -> bytes:
-        salt = bcrypt.gensalt()
-        hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
-        return hashed
-
-    def is_valid(hashed_password: bytes, password: str) -> bool:
-        return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
